98a5eb24fa811b934800a1a8c70e4da0be673089
Solomon <solomonuche@gmail.com>
2023-09-08 00:08:29 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     list = dir(hidden_4)
     for i in range(len(list)):
-        if(list[i][0] != '_'):
+        if list[i][0:2] != '__':
             print(list[i])

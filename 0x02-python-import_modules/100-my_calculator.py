5345ba45fda875775f12bf942a716d18ce1812c8
Solomon <solomonuche@gmail.com>
2023-09-08 02:39:34 +0100

    math operation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif argv[2] == '+':
-        print(f"{int(argv[1])} + {int(argv[3])} = " 
-                f"{add(int(argv[1]), int(argv[3]))}")
+        print(f"{int(argv[1])} + {int(argv[3])} = "
+              f"{add(int(argv[1]), int(argv[3]))}")
     elif argv[2] == "-":
         print(f"{int(argv[1])} - {int(argv[3])} = "
-                f"{sub(int(argv[1]), int(argv[3]))}")
+              f"{sub(int(argv[1]), int(argv[3]))}")
     elif argv[2] == '*':
         print(f"{int(argv[1])} * {int(argv[3])} = "
-                f"{mul(int(argv[1]), int(argv[3]))}")
+              f"{mul(int(argv[1]), int(argv[3]))}")
     elif argv[2] == "/":
         print(f"{int(argv[1])} / {int(argv[3])} = "
-                f"{div(int(argv[1]), int(argv[3]))}")
+              f"{div(int(argv[1]), int(argv[3]))}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)

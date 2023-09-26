d1603678b0c2cab0a29045d0cf00b5677ba42bf0
Solomon <solomonuche@gmail.com>
2023-09-26 23:46:50 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create and initilize and instance of a class."""
+
+
+class Square:
+    """
+    This class deines an empty class.
+    """
+    def __init__(self, size):
+        """
+        The __init__ method initializes the instance attribute of the class.
+
+        Args:
+            size: private instance attribute.
+        self.__size = size

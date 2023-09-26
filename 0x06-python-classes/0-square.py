0099b62f19b2ebc60f48a0dffd0ee0a22ccdf71d
Solomon <solomonuche@gmail.com>
2023-09-26 23:26:24 +0100

    an empty class square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""Create an empty string."""
 class Square:
     """
     This class deines an empty class.
 
     No attribute or method is created at the moment.
-    It uses pass as a placeholder..
+    It uses pass as a placeholder.
     """
     pass

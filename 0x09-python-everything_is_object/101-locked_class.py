b7cdafcf207cd8b5f659d78f34717e71f1cac1bb
Solomon <solomonuche@gmail.com>
2023-10-04 01:49:20 +0100

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+LockedClass representation
+"""
+
+
 class LockedClass:
+    """
+    Class definition
+    """
+
     def __setattr__(self, name, value):
+        """ Set atrribute """
+
         if name == "first_name":
             self.__dict__[name] = value
         else:

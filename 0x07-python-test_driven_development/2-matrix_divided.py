6175301a2c3267907ef740fc1899bf11bf321d91
Solomon <solomonuche@gmail.com>
2023-10-05 00:11:51 +0100

    Test deriven development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,7 @@ Divide a matrix module
 def matrix_divided(matrix=None, div=None, *args):
     """
     matrix_divided - a function that divides all elements of a matrix.
-    
+
     Returns a new matrix
     """
     """ validate that more than 2 arguments were not passed"""
@@ -16,8 +16,9 @@ def matrix_divided(matrix=None, div=None, *args):
                         " positional arguments, but ... were given")
 
     if matrix is None or div is None:
-        raise TypeError("matrix_divided() missing 2 required positional arguments")
-    
+        raise TypeError("matrix_divided() missing 2 required "
+                        "positional arguments")
+
     """ Validate div argument"""
     if not isinstance(div, (int, float)) and div is not None:
         raise TypeError("div must be a number")
@@ -25,12 +26,13 @@ def matrix_divided(matrix=None, div=None, *args):
         raise ZeroDivisionError("division by zero")
 
     _list = []
-    
+
     """ validate that each rows of the matrix is of equal length"""
     for i in range(len(matrix)):
         """ check that matrix is a list of list"""
         if not isinstance(matrix[i], list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) "
+                            "of integers/floats")
         if len(matrix[0]) != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
 
@@ -39,7 +41,8 @@ def matrix_divided(matrix=None, div=None, *args):
         row = []
         for item in rows:
             if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
             n = round(item / div, 2)
             row.append(n)
 

683cda82f158bb2deda137b2d2e2455823463d11
Solomon <solomonuche@gmail.com>
2023-10-05 01:46:10 +0100

    Test deriven development'

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Say my name
+"""
+
+
+def say_my_name(first_name=None, last_name="", *args):
+    """
+    a function that prints My name is <first name> <last name>
+    """
+
+    """ Validate that not more 2 arguments is passed into the functio"""
+    if len(args) != 0:
+        raise TypeError("say_my_name() expects 1 or 2 positional "
+                        "arguments, but ... were given")
+
+    """ Check that at least one argument is passed"""
+    if first_name is None:
+        raise TypeError("say_my_name() missing 1 required positional argument")
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")

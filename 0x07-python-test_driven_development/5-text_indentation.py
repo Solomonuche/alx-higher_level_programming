b048772decfc2f86dfb568b6762e4fabad3923dd
Solomon <solomonuche@gmail.com>
2023-10-07 14:24:29 +0100

    test deriven development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,6 +13,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    text = text.strip()
+
     _list = []
     for i in text:
         if i in ('.', '?', ':'):

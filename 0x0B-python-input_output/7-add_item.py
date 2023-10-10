7184c9aa63369e9b9c036938b48bc4a795bc407a
solomonuche <solomonuche@gmail.com>
2023-10-10 16:34:43 -0700

    load, add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,6 +11,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 mylist = list()
+
+try:
+    mylist = load_from_json_file("add_item.json")
+except Exception:
+    pass
 if len(argv) == 0:
     save_to_json_file(mylist, "add_item.json")
 else:

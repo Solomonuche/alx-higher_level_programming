40bcb58e0444f141a34989cacf9142add7dc8be0
Solomon <solomonuche@gmail.com>
2023-10-11 13:38:21 +0100

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Student to JSON Class representation
+"""
+
+
+class Student:
+    """ a class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initiliazer function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+         method that retrieves a dictionary
+         """
+
+         return Student.__dict__

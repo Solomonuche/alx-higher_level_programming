b869ebbe318ad212dba532cd5867bcc818189f0f
Solomon <solomonuche@gmail.com>
2023-10-17 01:03:39 +0100

    Test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Test_rectangle unit test"""
 import unittest
+from unittest.mock import patch
+import io
 from models.rectangle import Rectangle
 from models.base import Base
 
@@ -59,3 +61,74 @@ class TestRectangle(unittest.TestCase):
 
         r3 = Rectangle(8, 7, 0, 0, 12)
         self.assertEqual(r3.area(), 56)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_display(self, mock_stdout):
+        """Test display method"""
+
+        r1 = Rectangle(4, 6)
+        r1.display()
+
+        printed_output = mock_stdout.getvalue()
+
+        expected_output = "####\n####\n####\n####\n####\n####\n"
+
+        self.assertEqual(printed_output, expected_output)
+
+        """ Test display method with x, y parameter"""
+        """reset the mock_stdout"""
+
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+
+        r2 = Rectangle(2, 3, 2, 2)
+        r2.display()
+
+        printed_output = mock_stdout.getvalue()
+
+        expected_output = "\n\n  ##\n  ##\n  ##\n"
+
+        self.assertEqual(printed_output, expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_str(self, mock_stdout):
+        """Test __str__ method"""
+
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        print(r1)
+
+        printed_output = mock_stdout.getvalue()
+
+        expected_output = "[Rectangle] (12) 2/1 - 4/6\n"
+
+        self.assertEqual(printed_output, expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_update(self, mock_stdout):
+        """Test update method"""
+
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89)
+        print(r1)
+
+        printed_output = mock_stdout.getvalue()
+
+        expected_output = "[Rectangle] (89) 10/10 - 10/10\n"
+
+        self.assertEqual(printed_output, expected_output)
+
+        """ Test update method with **kwargs parameter"""
+        """reset the mock_stdout"""
+
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+        
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(x=1, height=2, y=3, width=4, id=89)
+        print(r1)
+
+        printed_output = mock_stdout.getvalue()
+
+        expected_output = "[Rectangle] (89) 1/3 - 4/2\n"
+
+        self.assertEqual(printed_output, expected_output)

2d674feedd2085785c9dcfd53d2fe47dd070fbbc
solomonuche <solomonuche@gmail.com>
2023-11-14 13:34:42 -0800

    add row to a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- a script that inserts a new row in the table first_table
-INSERT INTO first_table (id INT, name VARCHAR(256)) VALUES (id = 89, name = 'Best School')
+INSERT INTO first_table (id, name) VALUES (id = 89, name = 'Best School')

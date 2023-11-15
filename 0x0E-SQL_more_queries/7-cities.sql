60bba913114fddbb7fae12be59c5d0853de834b9
solomonuche <solomonuche@gmail.com>
2023-11-15 13:36:47 -0800

    create table with foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`state_id` INT NOT NULL,
-	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`state`(`id`),
+	FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`),
 	`name` VARCHAR(256) NOT NULL
 )

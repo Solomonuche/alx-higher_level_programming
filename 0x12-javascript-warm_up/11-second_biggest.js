f8e6091b14da19d768bdadb438312ed593601563
Solomon <solomonuche@gmail.com>
2023-12-12 22:40:28 +0100

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ const args = process.argv;
 
 if (args.length > 3) {
   const maxNumber = Math.max(...args.slice(2));
-  let secondLargest = 0;
+  let secondLargest = -Infinity;
 
   for (let i = 2; i < args.length; i++) {
     if (args[i] === maxNumber) continue;

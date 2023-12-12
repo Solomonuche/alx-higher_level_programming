c2649d9668e3cb837f638f1b664dfadc6ef0beb1
Solomon <solomonuche@gmail.com>
2023-12-12 23:19:51 +0100

    Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* a script that prints the addition of 2 integers
+ * The first argument is the first integer
+ * The second argument is the second integer
+ */
+
+const add = (a, b) => a + b;
+
+module.exports = { add };

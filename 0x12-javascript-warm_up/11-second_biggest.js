e9e59d37e84cedac32f980f5be7fdf056cf4a551
Solomon <solomonuche@gmail.com>
2023-12-12 22:33:00 +0100

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+//  a script that searches the second biggest integer in the list of arguments.
+
+const args = process.argv;
+
+if (args.length > 3) {
+  const maxNumber = Math.max(...args.slice(2));
+  let secondLargest = 0;
+
+  for (let i = 2; i < args.length; i++) {
+    if (args[i] === maxNumber) continue;
+    if (args[i] > secondLargest && args[i] < maxNumber) {
+      secondLargest = args[i];
+    }
+  }
+  console.log(secondLargest);
+} else {
+  console.log(1);
+}

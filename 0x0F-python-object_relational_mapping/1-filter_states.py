930ba295799915d354d087ff9f24d538016c6047
Solomon <solomonuche@gmail.com>
2023-12-15 00:31:33 +0100

    a script that lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb as DB
+
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+"""
+
+connect = DB.connect(user="root", passwd="", db="hbtn_0e_0_usa")
+cur = connect.cursor()
+cur.execute("SELECT id, name FROM states WHERE states.name LIKE 'N%' \
+        ORDER by states.id ASC")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+"""clean up"""
+cur.close()
+connect.close()

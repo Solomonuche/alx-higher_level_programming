30077348b4e3640249848dc2b965328419c186e3
Solomon <solomonuche@gmail.com>
2023-12-15 20:47:16 +0100

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,19 +7,16 @@ a script that lists all states from the database hbtn_0e_0_usa
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 """
-username = argv[1]
-password = argv[2]
-db_name = argv[3]
+if __name__ == "__main__":
+    connect = DB.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = connect.cursor()
+    cur.execute("SELECT id, name FROM states ORDER by states.id ASC")
+    rows = cur.fetchall()
 
-connect = DB.connect(host="localhost", port=3306, user=username,
-                     passwd=password, db=db_name)
-cur = connect.cursor()
-cur.execute("SELECT id, name FROM states ORDER by states.id ASC")
-rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-for row in rows:
-    print(row)
-
-"""clean up"""
-cur.close()
-connect.close()
+    """clean up"""
+    cur.close()
+    connect.close()

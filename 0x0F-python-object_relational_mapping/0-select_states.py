c6034b49189a610c758a843a7439c4278f385e0f
Solomon <solomonuche@gmail.com>
2023-12-15 20:55:22 +0100

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,8 @@ a script that lists all states from the database hbtn_0e_0_usa
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 """
+
+
 if __name__ == "__main__":
     connect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])

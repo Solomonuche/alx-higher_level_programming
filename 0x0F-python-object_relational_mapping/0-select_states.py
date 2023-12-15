8c10b2afc7350b463763f3c6f2343c55ca3a22b2
Solomon <solomonuche@gmail.com>
2023-12-15 20:23:12 +0100

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from sys import argv
 import MySQLdb as DB
 
 """
@@ -6,8 +7,12 @@ a script that lists all states from the database hbtn_0e_0_usa
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 """
+username = argv[1]
+password = argv[2]
+db_name = argv[3]
 
-connect = DB.connect(user="root", passwd="", db="hbtn_0e_0_usa")
+connect = DB.connect(host="localhost", port=3306, user=username,
+                     passwd=password, db=db_name)
 cur = connect.cursor()
 cur.execute("SELECT id, name FROM states ORDER by states.id ASC")
 rows = cur.fetchall()

e7953e83185159d21911794776cb7ecd009af9ef
Solomon <solomonuche@gmail.com>
2023-12-18 01:13:09 +0100

    Cities in state relationship

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a script that changes the name of a State object from the database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+import sys
+
+if __name__ == "__main__":
+    """connect to the server """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    """ Create session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ Query with relationship """
+    obj = session.query(City).join(State, City.state_id == State.id).\
+        order_by(City.id)
+
+    for row in obj:
+        print(f'{row.state.name}: {row.id} {row.name}')
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a python file that contains the class definition of a city and
+an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """
+    City class representation that inherits from class and mapp to a database
+    """
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, nullable=False, autoincrement=True,
+                unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    # Relationship
+    state = relationship('State', backref='city')

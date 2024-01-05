c5e27c6742c9e565d2522b04bef794d9cce9c724
Solomon <solomonuche@gmail.com>
2024-01-05 22:45:39 +0100

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sIo tmp.txt 0.0.0.0:5000 && sed -n '/Content-Length/p' tmp.txt | awk '{print $2}' && rm tmp.txt
+curl -sIo tmp.txt $1 && sed -n '/Content-Length/p' tmp.txt | awk '{print $2}' && rm tmp.txt

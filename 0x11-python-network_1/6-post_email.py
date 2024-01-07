5eb3ea5aa8ed024ca8ecd7dc732eb74347cff0d3
solomonuche <solomonuche@gmail.com>
2024-01-07 15:26:10 -0800

    requests post request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+POST an email
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {'email': argv[2]}
+    response = requests.post(argv[1], data=data)
+    print(response.text)

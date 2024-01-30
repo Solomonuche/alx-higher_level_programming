971820e3beb97fde3c26975a5ca908da4399efaa
Solomon <solomonuche@gmail.com>
2024-01-30 20:17:22 +0100

    a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that reads and prints the content of a file.
+
+const filename = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  else if (data) console.log(data);
+});
